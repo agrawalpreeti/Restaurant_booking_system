7ea54b6b7472b75a9903e0279cd3669ead380357
Vibs00 <vibha.ajm13@gmail.com>
2019-06-07 11:47:15 +0530

    carousel added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ControlledCarousel from './components/MainPage.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Main extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <ControlledCarousel></ControlledCarousel>
+    );
+  }
 }
 
-export default App;
+export default Main;
