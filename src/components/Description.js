2ea5a7344e5e9670392447f96888d0a084ba322f
agrawalpreeti <agrawalpreeti1998@gmail.com>
2019-06-23 19:42:42 +0530

    normal

--- src/components/Description.js
+++ src/components/Description.js
@@ -3,7 +3,6 @@ import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 import './Mycss.css';
 import { TabPane, TabContainer } from 'react-bootstrap';
-import { fontSize } from '@material-ui/system';
 
 
 
@@ -14,47 +13,21 @@ class Description extends React.Component {
         key: 'home',
       };
     }
+    // reviews = () => {
+    //  let rev=[]; 
+    //  rev= this.props.restaurantInfo.all_reviews.reviews.map((value, index) =>
+    //  <li>{value}</li>
+    //  );
+    //  return rev
+    // }
 
-    reviews = () => {
-     let rev=[]; 
-     rev = this.props.restaurantInfo.all_reviews.reviews.map((value) =>
-     <li style={{marginTop: '2%'}}>
-        <h4>
-          {value.review.user.name}
-        </h4>
-        <p>
-          {"(" + value.review.review_time_friendly + ")"}
-        </p>
-        <img  class="profile" alt="Profile pic" src={value.review.user.profile_image}></img>
-        <span style={{marginLeft: '60%'}}>{"(" + value.review.rating+"/5)"}</span>
-        <span style={{marginLeft: '60%'}} class="rating1">
-          <span>☆</span>
-        </span>
-        <em>
-        <p style={(value.review.rating>3) ? {fontSize:'20px', color: 'green'} : ((value.review.rating=3) ? {fontSize:'20px', color: '#C6C000'} : {fontSize:'20px', color: 'red'})}>
-          {value.review.rating_text}</p>
-          {value.review.review_text}
-        </em>
-      </li>
-            );
-     return rev;
-    }
-
-    photos = () =>{
-      let pics = [];
-      pics = this.props.restaurantInfo.photos.map((value)=>
-        <a href={value.photo.url} target= '_blank'><img style={{width:'100px', height:'100px', objectFit: 'cover'}} src={value.photo.url} alt="restaurant pics"/></a>
+   phone_numbers = () =>{
+      let number = this.props.restaurantInfo.phone_numbers.split(',');
+      let num = number.map((value)=>
+      <li>{value}</li>
       );
-      return pics;
+      return num;
     }
-
-  //  phone_numbers = () =>{
-  //     let number = this.props.restaurantInfo.phone_numbers.split(',');
-  //     let num = number.map((value)=>
-  //     <li>{value}</li>
-  //     );
-  //     return num;
-  //   }
   
     render() {
       return (
@@ -63,17 +36,22 @@ class Description extends React.Component {
           activeKey={this.state.key}
           onSelect={key => this.setState({ key })}
         >
-          <Tab eventKey="review" title="Reviews"> <br></br>
-          <ul>
-            {this.reviews()}
-          </ul>
+          <Tab eventKey="review" title="Review"> <br></br>
+          <b> {this.props.restaurantInfo.all_reviews.reviews[0].review.user.name}</b><br></br>
+
+          <img  class="profile" alt="Profile pic" src={this.props.restaurantInfo.all_reviews.reviews[0].review.user.profile_image} ></img><br></br>
+          {"Rating : "}
+         {this.props.restaurantInfo.all_reviews.reviews[0].review.rating+"/5"}
+        <strong> {this.props.restaurantInfo.all_reviews.reviews[0].review.rating_text}</strong>
+         <br></br>
+          <i>{this.props.restaurantInfo.all_reviews.reviews[0].review.review_text}</i><br></br>
+           {this.props.restaurantInfo.all_reviews.reviews[0].review.review_time_friendly}
+          
           </Tab>
-          <Tab eventKey="pictures" title="Pictures">
-            {this.photos()}
+          <Tab eventKey="profile" title="Profile">
           </Tab>
-          <Tab eventKey="menu" title="Menu"><br></br>
-          </Tab>
-          <Tab eventKey="direction" title="Direction"><br></br>
+          <Tab eventKey="contact" title="Contact"><br></br>
+          {this.phone_numbers()}
           </Tab>
         </Tabs>
       );
